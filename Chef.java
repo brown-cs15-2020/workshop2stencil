a561cae03c54e4ba7a8b5a5aea3cd3036d34ed12
abigail <abigail_marks@brown.edu>
2021-09-01 16:19:29 -0400

    updated the instance variable syntax

--- Chef.java
+++ Chef.java
@@ -1,18 +1,18 @@
 package workshop2stencil;
 
 public class Chef {
-    private String _signatureDish;
-    private int _signaturePrice;
+    private String signatureDish;
+    private int signaturePrice;
 
     // TODO: Create a constructor, initialize instance variables
 
     //TODO: create a cook method
 
     public void setSignatureDish(String dish){
-        _signatureDish = dish;
+        this.signatureDish = dish;
    }
     public String getSignatureDish(){
-        return _signatureDish;
+        return this.signatureDish;
     }
 
     // TODO: create accessor and mutator methods for _signaturePrice
--- Restaurant.java
+++ Restaurant.java
@@ -1,23 +1,23 @@
 package workshop2stencil;
 
 public class Restaurant {
-    private Chef _mainChef;
+    private Chef mainChef;
     //TODO: declare another Chef instance variable
     public Restaurant() {
-        _mainChef = new Chef("Andy");
+        this.mainChef = new Chef("Andy");
         // TODO: Create the other chef (initialize and instantiate)
         this.createMenu();
     }
     public void createMenu(){
-        _mainChef.setSignatureDish("cookies");
-        _mainChef.setSignaturePrice(6);
+        this.mainChef.setSignatureDish("cookies");
+        this.mainChef.setSignaturePrice(6);
 
         // TODO: use the getters and setters to add the other chefs signature dish
 
     }
 
     public void seeMenu(){
-        System.out.println(_mainChef.getSignatureDish() + ", "+ _mainChef.getSignaturePrice());
+        System.out.println(this.mainChef.getSignatureDish() + ", "+ this.mainChef.getSignaturePrice());
         // TODO: use the getters to print out the sous chefs signature dish
         // TODO: add another dish to the menu
         System.out.println("");
